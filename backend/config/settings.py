9b049fe5e573ea95526cefa1f046bde9a4c094b4
Shamim <hoshenshamim522@gmail.com>
2026-08-08 20:12:02 +0600

    Fix ALLOWED_HOSTS for Render

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -16,11 +16,7 @@ SECRET_KEY = 'django-insecure-eip_lwe83adab!8ci126uopsk4t8lwnzc13)^az34r3c=k!uhy
 
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    "127.0.0.1",
-    "localhost",
-]
-
+ALLOWED_HOSTS = ['campusconnect-backend-x3qe.onrender.com', 'localhost', '127.0.0.1']
 # =========================
 # APPS
 # =========================
