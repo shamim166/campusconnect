b5483db8c388bd94c4f1a37e5186800a3cd9b735
Shamim <hoshenshamim522@gmail.com>
2026-08-09 01:24:14 +0600

    Make EMAIL_HOST and EMAIL_PORT dynamic from env variables

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -172,8 +172,8 @@ CORS_ALLOW_CREDENTIALS = True
 # =========================
 if os.environ.get('EMAIL_HOST_USER'):
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_HOST = 'smtp.gmail.com'
-    EMAIL_PORT = 587
+    EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.gmail.com')
+    EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
     EMAIL_USE_TLS = True
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
